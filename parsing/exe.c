0e0a54224c5a2a9f5c947ab5d198ecbff4cfa426
jgirard- <julengirard@outlook.com>
2023-05-09 15:04:23 +0200

    cmd not found et exitcodes working

--- parsing/exe.c
+++ parsing/exe.c
@@ -151,7 +151,6 @@ void	executeur_final(char **s, char **env, t_var *var, t_lex *lex)
 		cmdpath = 0;
 	if (cmdpath == 0)
 	{
-		//printf("%d", g_global.exitcode);
 		if (s[0][1] == '.' && s[0][1] == '.' && s[0][2] == '/')
 			printf("minishell: no such file or directory: %s\n", s[0]);
 		else if (g_global.exitcode == 127)
